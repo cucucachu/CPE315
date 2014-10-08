0423e2bc76e14e4b2008ed6e6ddb1e8ca9446f3b
cucuchachu <codymjones@ymail.com>
2014-10-08 16:02:28 -0700

    More work on lab 2

--- Lab2/Assembler.java
+++ Lab2/Assembler.java
@@ -6,11 +6,57 @@ public class Assembler {
 	public static void assemble(File input) throws FileNotFoundException {
 		Scanner scanner = new Scanner(input);
 		String cur;
-		while (scanner.hasNext()) {
-			cur = scanner.next();
+		while (scanner.hasNextLine()) {
+			cur = scanner.nextLine();
 			if (cur.charAt(0) == '$') {
 				System.out.println(cur);
 			}
 		}	
 	}
+	
+	public static String translateLine(String line) 
+		throws BadInstructionException, RegNotFoundException, SyntaxException {
+			int comment;
+			int type;
+			String inst;
+			String machineCode;
+			Scanner scanner;
+			
+			
+			machineCode = "";
+			comment = line.indexOf("#");
+			
+			if (comment != -1) 
+				line = line.substring(0, comment);
+			
+			scanner = new Scanner(line);
+			
+			if (!scanner.hasNext())
+				return machineCode;
+			
+			try {
+				inst = scanner.next();
+			}
+			catch (Exception ex) {
+				throw new SyntaxException("No instruction");
+			}
+			
+			type = Instructions.getType(inst);
+			
+			if (type == Instructions.RTYPE) {
+			
+			}
+			else if (type == Instructions.ITYPE) {
+			
+			}
+			else if (type == Instructions.JTYPE) {
+			
+			}
+			else
+				throw new SyntaxInstruction("Bad Instruction " + inst);
+			
+			return machineCode;
+	}
 }
+
+
--- Lab2/Instructions.java
+++ Lab2/Instructions.java
@@ -1,4 +1,45 @@
 public class Instructions {
+	public static final int RTYPE = 0;
+	public static final int ITYPE = 1;
+	public static final int JTYPE = 2;
+
+	public static int getType(String inst) throws BadInstructionException {
+		inst = inst.toLowerCase();
+		inst = inst.trim();
+		
+		if (inst.compareTo("and") == 0)
+			return RTYPE;
+		else if (inst.compareTo("or") == 0)
+			return RTYPE;
+		else if (inst.compareTo("add") == 0)
+			return RTYPE;
+		else if (inst.compareTo("addi") == 0)
+			return ITYPE;
+		else if (inst.compareTo("sll") == 0)
+			return ITYPE;
+		else if (inst.compareTo("sub") == 0)
+			return RTYPE;
+		else if (inst.compareTo("slt") == 0)
+			return RTYPE;
+		else if (inst.compareTo("beq") == 0)
+			return ITYPE;
+		else if (inst.compareTo("bne") == 0)
+			return ITYPE;
+		else if (inst.compareTo("sw") == 0)
+			return ITYPE;
+		else if (inst.compareTo("lw") == 0)
+			return ITYPE;
+		else if (inst.compareTo("j") == 0)
+			return JTYPE;
+		else if (inst.compareTo("jr") == 0)
+			return RTYPE;
+		else if (inst.compareTo("jal") == 0)
+			return JTYPE;
+		else
+			throw new BadInstructionException("Could not find type for " + inst + ".");
+	
+	}
+
 	public static String getOpCode(String inst) throws BadInstructionException {
 		inst = inst.toLowerCase();
 		inst = inst.trim();
@@ -70,4 +111,6 @@ public class Instructions {
 		else
 			throw new BadInstructionException("Could not find opCode for " + inst + ".");
 	}
+	
+	public static 
 }
